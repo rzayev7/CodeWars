30eddcbeae43aaa7a5659b5d25fb71f786035177
Sahib Rzayev <rzaevsahib@gmail.com>
2024-03-05 11:17:04 +0400

    Initial commit

---
+++ Tribonacci.js
@@ -0,0 +1,14 @@
+
+
+
+let signature=[1,1,1];
+let n=1;
+
+function tribonacci(signature,n){  
+    for (var i = 0; i < n-3; i++) { 
+      signature.push(signature[i] + signature[i+1] + signature[i+2]); 
+    }
+    return signature.slice(0, n); 
+  }
+
+console.log(tribonacci(signature,n));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+if(n<3){
+        let array=[];
+        for (let i = 0; i < n; i++) {
+            array.push(signature[i]);
+        }
+        return array;
+    }
